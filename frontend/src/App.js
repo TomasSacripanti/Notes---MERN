e62ea34fd6b862b2b96910db8a501948e3703456
Tomás Sacripanti <sacripantitomas@gmail.com>
2020-11-26 16:14:28 -0300

    Creación del frontend y config de React y React Router

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router , Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+
+import Navigation from './components/Navigation';
+import NotesList from './components/NotesList';
+import CreateUser from './components/CreateUser';
+import CreateNote from './components/CreateNote';
+
+
+function App() {
+  return (
+    <Router>
+      <Navigation />
+      <Route path="/" exact component={NotesList}></Route>
+      <Route path="/edit/:id" component={CreateNote}></Route>
+      <Route path="/create" component={CreateNote}></Route>
+      <Route path="/user" component={CreateUser}></Route>
+    </Router>
+  );
+}
+
+export default App;
